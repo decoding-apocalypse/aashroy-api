f29045a5bf55d699bd0115b0c812a1158179bf2e
Vikas <vikassuthar528@gmail.com>
2021-09-23 01:27:38 +0530

    added mailer and used algorithm in upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const uploadUserData = require("../models/Upload");
+const mailer = require("../utils/mailer");
+const shortestDistance = require("../helper/shortestDistance");
+const ngosData = require("../helper/ngosData");
 
 router.get("/", async (req, res) => {
   try {
@@ -42,18 +45,54 @@ router.post("/", async (req, res) => {
       date: new Date(),
     });
     const uploadData = await newUploadUserData.save();
+    console.log(uploadData);
     // mail to NGO using algorithm
+    const particularNgoMailId = shortestDistance(ngosData.NGO, uploadData.location).email;
+    console.log(particularNgoMailId);
+    emailContent = {
+      subject: "message from aashroy",
+      html: `Hello 
+              We have received the following update uploaded by Mr/Mrs ${uploadData.username} regarding homeless people in your <br>
+              locality. The following details are true and same as uploaded in AASHROY.
+              <br> <br>
+              Homeless name: 
+              Homeless location: ${uploadData.location}
+              Age:
+              Need any sort of medical attention: ${uploadData.desciption}
+              Timestamp: ${uploadData.date}
+              <br> <br>
+              
+              We here at AASHROY believe your organization would be the best place for the individual to get the best help
+              and care he would require. Thanking you in advance for this noble pursuit.
+
+              <br> <br>
+              With regards,
+              TEAM AASHROY`,
+    };
+    const mailResponse = mailer(particularNgoMailId, emailContent);
+    console.log(mailResponse);
     res.status(201).json(uploadData);
   } catch (error) {
     res.json(error);
   }
 });
 
-// router.get("/:userId", async (req, res) => {
-//   const particularUserData = await uploadUserData.find(req.params.userId);
-//   console.log(req.session);
-//   res.json(particularUserData);
-//   // res.send(`Get request for upload ${req.body.userId}`);
-// });
+router.get("/:userId", async (req, res) => {
+  const particularUserData = await uploadUserData.findOne({
+    userId: req.params.userId,
+  });
+
+  if (!particularUserData) {
+    return res.json({
+      error: "User not found",
+    });
+  }
+
+  try {
+    res.json(particularUserData);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 module.exports = router;
