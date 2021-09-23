654f602d9733ba90776724fa5781838655e9c359
Vikas <vikassuthar528@gmail.com>
2021-09-23 21:10:51 +0530

    added donation/stuffs post route and corrected logic error

--- routes/donation.js
+++ routes/donation.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const ngosData = require("../helper/ngosData");
+const shortestDistanceNGO = require("../helper/shortestDistance");
 const router = express.Router();
 const Donation = require("../models/Donation");
+const mailer = require("../utils/mailer");
 
+// get request for all donations
 router.get("/", async (req, res) => {
   try {
     const allDonations = await Donation.find({});
@@ -11,18 +15,35 @@ router.get("/", async (req, res) => {
   }
 });
 
-
-
 // stuffs post req to send email to NGOs using algorithm
+router.post("/stuffs", async (req, res) => {
+  const { donorDetails, items, location, date} = req.body;
+  const userLocation = {
+    lat: location.latitude,
+    lng: location.longitude
+  }
+  const nearestNgo = shortestDistanceNGO(ngosData.NGO, userLocation);
+  // console.log(nearestNgo);
+  const emailContent  = {
+    subject: "",
+    html: `
+          Dear <NGO name>
+          MR <> wants to send food/clothes/medicine or similar commodities to help your organization in the minimalistic way possible. His contact and address details as uploaded are :
+    
+          Name:
+          Location:
+          Commodity: food/medicine/clothes
+          Preferable pickup date:
+          Contact no:
+    
+          We here at AASHROY believe that every drop makes an ocean and each help makes a difference in making the world a better place to live in.
+    
+          With regards,
+          TEAM AASHROY`,
+  }
+  res.json(mailer(nearestNgo.email, emailContent));
+});
 
-// get request for all donations
+
 
 module.exports = router;
-
-
-
-
-
-
-
-
